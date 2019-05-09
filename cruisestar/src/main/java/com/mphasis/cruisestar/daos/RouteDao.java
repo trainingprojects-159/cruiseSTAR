2a197284cd964d18b8c07982bfaa80b60fae3cb9
supriya.das <supriya.das@WKSBAN24TR17004.corp.mphasis.com>
2019-05-09 15:48:28 +0530

    corrected route .

--- cruisestar/src/main/java/com/mphasis/cruisestar/daos/RouteDao.java
+++ cruisestar/src/main/java/com/mphasis/cruisestar/daos/RouteDao.java
@@ -9,7 +9,7 @@ public interface RouteDao {
 	public void addRoute(Route route);
 	public void editRoute(Route route);
 	public void deleteRoute(int routeid);
-	public Route getAllRoutes();
-	public void getRouteById(int routeid);
+	public List<Route> getAllRoutes();
+	public Route getRouteById(int routeid);
 	public List<Route> viewSchedule(String source,String destination);
 }
