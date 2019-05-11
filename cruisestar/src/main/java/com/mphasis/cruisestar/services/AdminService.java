96114d99af3f61cee0049e1d16eb58dddc48b696
rakesh.s1 <rakesh.s1@WKSBAN24TR17028.corp.mphasis.com>
2019-05-11 12:39:13 +0530

    deleting the old project

--- cruisestar/src/main/java/com/mphasis/cruisestar/services/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.mphasis.cruisestar.services;
-
-import com.mphasis.cruisestar.entities.Admin;
-
-public interface AdminService {
-	
-	public Admin adminlogin(String adminname,String password);
-
-}
